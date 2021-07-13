2a708970b0494cb9225fbdab79b8ff6b5c9b6af5
anggiatpangaribuan28 <anggiatpangaribuan28@gmail.com>
2021-07-13 16:37:36 +0700

    Module 2 Session1 New

--- Module 2/Session 1/021/src/App.js
+++ Module 2/Session 1/021/src/App.js
@@ -5,12 +5,42 @@ import data from './data'
 function App() {
   const {album, artists} = data;
   return (
-    <div>
-      <h1>Playlist</h1>
-    <p>  Album : {album.name}</p>
-    <p>  Nama : {artists[0].name}</p>
-    <p>  Gambar : <img src={album.images[1].url} /> </p>
-
+    <div className="App">
+      <h1>Playlist on spotify</h1>
+      <table className="table-playlist">
+        <tr>
+          <td rowsSpan>
+              <img src={album.images[2].url} />
+          </td>
+          <td>
+              <tr>
+                  <td>
+                     <h2>{album.name}</h2>
+                  </td>
+              </tr>
+          <tr>
+        <td>
+          <h3>{artists[0].name}</h3>
+          </td>
+        </tr>
+          </td>
+        </tr>
+      </table>
+      <style jsx>
+        {`
+        .App{
+            background-color:#879872;
+            padding:10px;
+            height:100vh;
+        }
+        .table-playlist{
+          background-color: #68687f;
+          padding:10px;
+          width:100%;
+          margin:0px 10px 0 10px;
+        }
+        `}
+      </style>
     </div>
   )
 }
